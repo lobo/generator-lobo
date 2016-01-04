f03507f343c621d34ba4f68bdae92e228ec8385e
Daniel Lobo <dlobo@itba.edu.ar>
2016-01-04 03:32:08 -0300

    Updated schema

--- generators/schema/templates/_schema.js
+++ generators/schema/templates/_schema.js
@@ -1,15 +1,41 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-		Schema = mongoose.Schema,
-		ObjectId = Schema.ObjectId;
+var mongoose = require('mongoose');
+var	Schema   = mongoose.Schema;
+var	ObjectId = Schema.ObjectId;
 
-var fields = {
+var <%= lowSchemaName %>Schema  = mongoose.Schema({
 <% schemaFields.forEach(function(field, index) {
 	switch(field.split(":")[1]){
-		case 'String':  %>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; case 'Number':  %>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; case 'Date':  %>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " , default: Date.now }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; case 'Buffer':  %>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; case 'Boolean': %>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; case 'Mixed': %>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; case 'ObjectId': 	%>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; case 'Array': %>	<%= field.split(":")[0] + ": { type: " + field.split(":")[1] + " }" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; } }) %>
-};
+	case 'String':  %>	<%= field.split(":")[0] + ": {\n\t\ttype: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break;
+	  case 'Number':  %>	<%= field.split(":")[0] + ": {\n\t\ttype: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break;
+	  case 'Date':  %>	<%= field.split(":")[0] + ": {\n\t\ttype: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " , default: Date.now \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break;
+	  case 'Buffer':  %>	<%= field.split(":")[0] + ": {\n\t\t type: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break;
+	  case 'Boolean': %>	<%= field.split(":")[0] + ": {\n\t\ttype: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break;
+	  case 'Mixed': %>	<%= field.split(":")[0] + ": {\n\t\ttype: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break;
+	  case 'ObjectId': %>	<%= field.split(":")[0] + ": {\n\t\ttype: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break;
+	  case 'Array': %>	<%= field.split(":")[0]	+ ": {\n\t\ttype: " + field.split(":")[1] + ",\n\t\t" + "required: true,\n\t\t" + "unique: false" + " \n\t}" %><% if(schemaFields.length - 1 !== index ){ %>,<%= "\n" %><% } break; } }) %>,
 
-var <%= lowSchemaName %>Schema = new Schema(fields);
+	// timestamp
+	created_at: {
+		 type: Date,
+		 default: Date.now
+	},
+
+	updated_at: {
+		 type: Date,
+		 default: Date.now
+	}
+});
+
+
+
+// pre ======================
+<%= lowSchemaName %>Schema.pre('save', function(next) {
+  now = new Date();
+  this.updated_at = now;
+
+  next();
+});
 
 module.exports = mongoose.model('<%= capSchemaName %>', <%= lowSchemaName %>Schema);
